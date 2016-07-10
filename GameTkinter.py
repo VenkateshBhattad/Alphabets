ecf40e306a3da946a90fc56fe07ee7285b116c18
Venkatesh Bhattad <Venkatesh Bhattad>
2016-07-10 22:38:50 +0530

    Added Voice

--- GameTkinter.py
+++ GameTkinter.py
@@ -1,17 +1,21 @@
 from Tkinter import *
 import tkFont
 import pyttsx
-engine = pyttsx.init()
-engine.say('Sally sells seashells by the seashore.')
-engine.say('The quick brown fox jumped over the lazy dog.')
-engine.runAndWait()
 def onKeyPress(event):
     label.configure(text=event.char)
+    talkTheChar(event.char)
+    
+def talkTheChar(str):
+    engine = pyttsx.init()
+    engine.setProperty('rate', 100)
+    engine.setProperty('voice', 'HKEY_LOCAL_MACHINE\SOFTWARE\Microsoft\Speech\Voices\Tokens\TTS_MS_EN-US_ZIRA_11.0')
+    engine.say(str)
+    engine.runAndWait()
 root = Tk()
 root.wm_title("Alphabet")
 customFont = tkFont.Font(family="Helvetica", size=100)
 var = StringVar()
-label = Label( root, width=10, height=5, font=customFont, bg="Green", fg="Red" )
+label = Label( root, width=10, height=5, font=customFont, text="A", bg="Green", fg="Red" )
 root.bind("<KeyPress>", onKeyPress)
 label.pack()
 root.mainloop()
